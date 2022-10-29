25d6f36e814e322809c66331b172c043e166165b
Dana Bradley <dbradley0909@outlook.com>
2022-10-29 03:39:01 -0400

    first commit

---
+++ src/clockpanejavafx/ClockPane.java
@@ -0,0 +1,17 @@
+package clockpanejavafx;
+
+public class ClockPane {
+
+    public String getHour() {
+        return null;
+    }
+
+    public String getMinute() {
+        return null;
+    }
+
+    public String getSecond() {
+        return null;
+    }
+
+}
